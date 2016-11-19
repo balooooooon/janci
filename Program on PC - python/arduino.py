264ecbf614153175f0e78fe51177c2557f3d9993
xpanis <panis1994@gmail.com>
2016-11-19 20:45:03 +0100

    Úprava formátu
    *upravenie polí objektov na objekt v objekte

--- Program on PC - python/arduino.py
+++ Program on PC - python/arduino.py
@@ -5,25 +5,34 @@ import requests
 
 update = {}
 update['data'] = {}
-update['data']['location'] = {}
-update['data']['temperature'] = {}
 
-ser = serial.Serial('COM5', 9600, timeout=0)
+
+ser = serial.Serial('COM5', 9600, timeout=0.1)
 while 1:
 	try:
 		line=(ser.readline().decode("utf8"))
 		if (line != ""):
+			update['data']['parameters'] = []
 			print(line)
 			line_parsed = line.split(",")
-			update['type'] = "updateMsg"
+			update['flightHash'] = '0cc175b9c0f1b6a831c399e269772661'
 			update['data']['time'] = line_parsed[0]
-			update['data']['location']['x'] = line_parsed[4]
-			update['data']['location']['y'] = line_parsed[5]
-			update['data']['location']['z'] = line_parsed[6]
-			update['data']['temperature']['in'] = line_parsed[3]
-			update['data']['temperature']['out'] = line_parsed[2]
-			update['data']['isburst'] = line_parsed[7]
+			test1 = {}
+			test1['values'] = {}
+			test1['type'] = 'position'
+			test1['values']['lat'] = line_parsed[4]
+			test1['values']['lng'] = line_parsed[5]
+			test1['values']['alt'] = line_parsed[6]
+			test1['timestamp'] = line_parsed[0]
+			update['data']['parameters'].append(test1)
+			test2 = {}
+			test2['values'] = {}
+			test2['type'] = 'temperature'
+			test2['values']['in'] = line_parsed[2]
+			test2['values']['out'] = line_parsed[1]
+			update['data']['parameters'].append(test2)
 			json_data = json.dumps(update)
+			print(json_data)
 			response = requests.post("http://posttestserver.com/post.php", json=json_data)
 	except ser.SerialTimeoutException:
 		print('Data could not be read')
