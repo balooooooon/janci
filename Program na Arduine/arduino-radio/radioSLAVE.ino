d77f06c7bde28cbe9f4fd1ae4904282af1ad0e5c
Jakub Findura <findurajakub@gmail.com>
2017-09-01 21:40:06 +0200

    Fixed CRC calculation, we don't discriminate any characters

--- Program na Arduine/arduino-radio/radioSLAVE.ino
+++ Program na Arduine/arduino-radio/radioSLAVE.ino
@@ -90,8 +90,7 @@ uint16_t gps_CRC16_checksum (char *string) {
  
   crc = 0xFFFF;
  
-  // Calculate checksum ignoring the first two $s
-  for (i = 2; i < strlen(string); i++) {
+  for (i = 0; i < strlen(string); i++) {
     c = string[i];
     crc = _crc_xmodem_update (crc, c);
   }
